32887e19a90c7ad7e77e3f6f92d8d0a9dd159ed8
Abrar Khan <heyabrarkhan@gmail.com>
2023-01-19 13:19:33 +0530

    Delete Request Achieved

--- Routes/Bugs.routes.js
+++ Routes/Bugs.routes.js
@@ -6,8 +6,9 @@ const {BugsModel} = require('../Models/Bugs.model');
 
 BugsRouter.get('/', async (req,res)=>{
     const {category} = req.query;
+    const {userID} = req.body;
     try {
-        const AllBugs = await BugsModel.find({category});
+        const AllBugs = await BugsModel.find({category,userID});
         res.send(AllBugs)
     } catch (error) {
         res.status(400).send({"message" : "Oops!! Something Went Wrong!!"})
@@ -25,7 +26,22 @@ BugsRouter.post('/createbug', async (req,res) =>{
     } catch (error) {
         res.status(400).send({"message" : "Opps! Error In Adding Bug"})
     }
+});
 
+BugsRouter.delete('/delete/:id', async (req,res) =>{
+    const {userID} = req.body;
+    const {id} = req.params;
+    const Bug = await BugsModel.findOne({_id : id});
+    if(userID !== Bug.userID) res.status(400).send({"message" : "Looks like you have not logged in, Please Login"})
+    else
+    {
+        try {
+            await BugsModel.findByIdAndDelete({_id : id});
+            res.send({"message" : "Bug Deleted Successfully"});
+        } catch (error) {
+            res.status(400).send({"message" : "Something Went Wrong"});
+        }
+    }
 })
 
 
